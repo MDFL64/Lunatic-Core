2ed087ea9b16af61646051d077258a1722059ed4
Adam Coggeshall <dameerkat@gmail.com>
2017-03-22 20:52:28 -0600

    Threading fixed.

--- kernel/src/lj_shim.c
+++ kernel/src/lj_shim.c
@@ -21,8 +21,3 @@ LUALIB_API int luaopen_debug(lua_State *L) {
 LUALIB_API int luaopen_jit(lua_State *L) {
 	return 0;
 }
-
-// this is just an exception handler, it should never actually get called.
-void lj_err_unwind_win(void* rec,void* cf, void* ctx, void* dispatch) {
-	write_str_halt("Windows exception handler called. WTF?\n");
-}
